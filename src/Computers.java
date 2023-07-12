afa4591974c73213f038cd6439c2dc3df7f9e3f9
MishMarchenko <mishmar4enko@gmail.com>
2023-07-12 13:06:46 +0300

    last part

---
+++ src/Computers.java
@@ -0,0 +1,19 @@
+public class Computers<String, V> {
+    private String os;
+    private V version;
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public void setVersion(V version) {
+        this.version = version;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Computers " +
+                "os = " + os +
+                ", version = " + version;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("Part one!");
+        List<Dog> collectionFirst = new ArrayList<>();
+        collectionFirst.add(new Dog());
+        collectionFirst.add(new Dog());
+
+        Set<Cat> collectionSecond = new HashSet<>();
+        collectionSecond.add(new Cat());
+        collectionSecond.add(new Cat());
+
+        animalSpeak(collectionFirst);
+        animalSpeak(collectionSecond);
+
+        System.out.println("Part two!");
+        Computers<String, Integer> hp = new Computers<>();
+        Computers<String, Double> macOS = new Computers<>();
+        hp.setOs("Windows");
+        hp.setVersion(10);
+        macOS.setOs("Ventura");
+        macOS.setVersion(13.2);
+        System.out.println(hp.toString());
+        System.out.println(macOS.toString());
+    }
+    public static void animalSpeak(Collection<? extends Animal> collection){
+        System.out.println("Animal say Hello");
     }
 }
